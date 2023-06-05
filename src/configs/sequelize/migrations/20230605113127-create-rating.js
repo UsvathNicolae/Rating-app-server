133c6b8fea016d3a7818cb95d205bba05905512e
Usvath Nicolae <nicu_usvath@yahoo.com>
2023-06-05 15:16:53 +0300

    Sequelize config and db migrations

---
+++ src/configs/sequelize/migrations/20230605113127-create-rating.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Ratings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      stars: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      img: {
+        type: Sequelize.BLOB('long'),
+        allowNull: false,
+      },
+      licencePlate: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      anonymous: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Ratings');
+  }
+};
---
+++ src/configs/sequelize/migrations/20230605113811-relations.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn("Ratings","userId",{type: Sequelize.INTEGER, references: {model: "Users", key: "id"}});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn("Ratings","userId");
+  }
+};
